5cc2a4d832ab0d50e391ba216fc2b4bc6983a46b
Bruno Rino <bruno.rino@gmail.com>
2016-01-10 15:35:43 +0100

    move glossary to django-apps repo

--- glossary/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.EntryListView.as_view(), name='list'),
-    url(r'^(?P<slug>.+)?/$', views.EntryDetailView.as_view(), name='detail'),
-]
